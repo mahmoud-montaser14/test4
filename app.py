09cd41153983ddfeb7692a08a9edfdc21fe0e29f
mahmoud-montaser14 <m.m.montaser14@gmail.com>
2024-12-08 03:25:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,5 @@ def api_predict():
     else:
         app.logger.error("Invalid file type.")
         return jsonify({'error': 'Invalid file type. Please upload a valid image.'}), 400
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run()
